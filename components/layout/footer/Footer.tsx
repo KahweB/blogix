addc6cafa3a74f7f9d4db884ad0f6c186e1dd3d9
Bora Deniz Bas <boradenizbas1@gmail.com>
2025-05-15 18:35:53 +0300

    commit before react-icons

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/ui/button'
+
 import Link from 'next/link'
 import React from 'react'
 
@@ -6,8 +8,25 @@ const Footer = () => {
         <div className='bg-primarycolor text-white py-6'>
             <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
                 <div className='text-center md:text-left'>
-                    <Link href="/" className='text-exl text-white font-bold'>Logo</Link>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, necessitatibus praesentium </p>
+                    <Link href="/" className='text-exl text-white font-bold space-y-4'>Logo</Link>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, necessitatibus praesentium</p>
+                </div>
+                <nav className='hidden md:flex space-x-6'>
+                    <Link href="/" className='text-white font-bold hover:underline'>Home</Link>
+                    <Link href="/about" className='text-white font-bold hover:underline'>About</Link>
+                    <Link href="/blog" className='text-white font-bold hover:underline'>Blog</Link>
+                    <Link href="/contact" className='text-white font-bold hover:underline'>Contact</Link>
+                </nav>
+                <div>
+                    <Button>
+
+                    </Button>
+                    <Button>
+
+                    </Button>
+                    <Button>
+
+                    </Button>
                 </div>
             </div>
 
