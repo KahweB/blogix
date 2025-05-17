f6a8393876d4b7eb9cc1c2e690b943db5dd0444e
Bora Deniz Bas <boradenizbas1@gmail.com>
2025-05-18 00:24:44 +0300

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
 import { Mona_Sans } from "next/font/google"
+import { siteMetadata } from "@/lib/siteMetadata";
 
 //==============FONTS START===================
 
@@ -23,12 +24,44 @@ const geistMono = Geist_Mono({
 
 //==============FONTS END===================
 
+//==============METADATA START==============
 export const metadata: Metadata = {
-  title: "Blogix",
-  description: "next js practice project",
+  title: {
+    template: `%s | ${siteMetadata.title}`,
+    default: siteMetadata.title
+  },
+  description: siteMetadata.description,
+  openGraph: {
+    title: siteMetadata.title,
+    description: siteMetadata.description,
+    url: siteMetadata.siteUrl,
+    siteName: siteMetadata.title,
+    images: [siteMetadata.socialBanner],
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteMetadata.title,
+    images: [siteMetadata.socialBanner],
+  },
+  metadataBase: new URL("http://localhost:3000"),
+
 };
 
-
+//==============METADATA END================
 export default function RootLayout({
   children,
 }: Readonly<{
